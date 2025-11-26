d60a06a81b91f52a19a8c1a9f39279045bba05cc
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-11-26 10:48:36 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
 });
 
 // Inicia o servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Backend server running on port ${PORT}`);
 });
