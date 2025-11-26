adc8839885154f99ecc460777029a0da25b5f9b1
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-11-26 10:05:44 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    host: "::",
+    port: 8080,
+    allowedHosts: ["pastelariapastelecana2025.onrender.com"],
+  },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
